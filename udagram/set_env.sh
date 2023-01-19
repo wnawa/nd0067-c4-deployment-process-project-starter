dcd0b47bbc94be15ed39ebe90ecbf5e5ccde2334
walaa nawar <walaa_nawar86@yahoo.com>
2023-01-19 17:40:31 +0100

    env

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -3,9 +3,9 @@
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST=database-1.c9mi2ey2m0e7.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=walaa-env.eba-qekg3xb9.us-east-1.elasticbeanstalk.com
+export POSTGRES_DB=database-1
+export AWS_BUCKET=walaabucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://walaabucket.s3-website-us-east-1.amazonaws.com
+export URL=http://walaa-env.eba-qekg3xb9.us-east-1.elasticbeanstalk.com
