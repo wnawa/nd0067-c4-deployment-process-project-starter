9908af7d7564e0577ecdae7128913bbe6d29511a
walaa nawar <walaa_nawar86@yahoo.com>
2023-01-19 14:37:30 +0100

    last

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.c9mi2ey2m0e7.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=walaa-env.eba-qekg3xb9.us-east-1.elasticbeanstalk.com
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://walaabucket.s3-website-us-east-1.amazonaws.com/
--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,10 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
+export const sequelize = new Sequelize(
+  
+  "postgres://postgres:postgres@database-1.c9mi2ey2m0e7.us-east-1.rds.amazonaws.com:5432/postgres"
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+
+  
+);
